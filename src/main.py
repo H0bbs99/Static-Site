516918fbcfb6d0a163797a30c8fc980a7a252037
h0bbs99 <jdoakes13@gmail.com>
2024-10-15 20:33:44 -0500

    Ch2 Nodes step 1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from textnode import TextNode, TextType
+
 
 def main():
-    print("hello world")
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
 
     
