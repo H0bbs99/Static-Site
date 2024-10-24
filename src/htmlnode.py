fbcff691af8cda2a1d89fa6ea3861a873955c3b5
H0bbs99 <jdoakes13@gmail.com>
2024-10-24 10:04:21 -1000

    partial complete

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,3 +31,14 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("No Tag")
+        if self.children == None:
+            raise ValueError("No Child")
+        
